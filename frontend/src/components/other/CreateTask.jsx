156603f34215a60ebdccd5a25eaf7a4ad526f9a8
Akhilesh Aher <aherakhilesh88@gmail.com>
2026-06-18 22:33:17 +0530

    Create Task Card Form Handling

--- frontend/src/components/other/CreateTask.jsx
+++ frontend/src/components/other/CreateTask.jsx
@@ -1,30 +1,49 @@
+import { useState } from "react";
 
 
 const CreateTask = () => {
+
+    const [taskTitle, setTaskTitle] = useState("");
+    const [taskDescription, setTaskDecriptiom] = useState("");
+    const [taskDate, setTaskDate] = useState("");
+    const [assignedTo, setAssignedTo] = useState("");
+    const [category, setCategory] = useState("");
+
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        console.log(taskTitle, taskDate, taskDescription, category, assignedTo);
+        setTaskTitle("");
+        setTaskDate("");
+        setAssignedTo("");
+        setTaskDecriptiom("");
+        setCategory("")
+    }
+
     return (
 
         <div>
-            <form className="flex w-full bg-mist-900 items-center justify-between mr-6 mt-10 text-white">
+            <form className="flex w-full bg-mist-900 items-center justify-between mr-6 mt-10 text-white" onSubmit={(e) => submitHandler(e)}>
                 <div className="w-1/2 flex flex-col mr-30 m-5 gap-4">
 
                     <div>
                         <h3 className="text-lg font-medium pb-1">Task Title</h3>
-                        <input type="text" className="border-2 border-gray-200 w-full p-2 rounded" placeholder="Make a UI Design" />
+                        <input type="text" className="border-2 border-gray-200 w-full p-2 rounded" placeholder="Make a UI Design" value={taskTitle} onChange={(e) => setTaskTitle(e.target.value)} />
                     </div>
 
                     <div>
                         <h3 className="text-lg font-medium pb-1">Date</h3>
-                        <input type="date" className="border-2 border-gray-200 w-full p-2 rounded" />
+                        <input type="date" className="border-2 border-gray-200 w-full p-2 rounded" value={taskDate} onChange={(e) => setTaskDate(e.target.value)} />
                     </div>
 
                     <div className="te">
                         <h3 className="text-lg font-medium pb-1">Assign To</h3>
-                        <input type="text" className="border-2 border-gray-200 w-full p-2 rounded" placeholder="Employee Name" />
+                        <input type="text" className="border-2 border-gray-200 w-full p-2 rounded" placeholder="Employee Name" value={assignedTo} onChange={(e) => setAssignedTo(e.target.value)} />
                     </div>
 
                     <div>
                         <h3 className="text-lg font-medium pb-1">Category</h3>
-                        <input type="text" className="border-2 border-gray-200 w-full p-2 rounded" placeholder="design, dev, etc" />
+                        <input type="text" className="border-2 border-gray-200 w-full p-2 rounded" placeholder="design, dev, etc" value={category} onChange={(e) => setCategory(e.target.value)} />
                     </div>
 
                 </div>
@@ -32,7 +51,7 @@ const CreateTask = () => {
                 <div className="w-1/2 flex flex-col">
                     <h3 className="text-lg font-medium pb-1">Description</h3>
                     <textarea name="" id="" className="border-2 border-gray-100 rounded" rows={10}></textarea>
-                    <button className="w-full bg-emerald-400 p-3 mt-2 rounded-sm">Create Task</button>
+                    <button className="w-full bg-emerald-400 p-3 mt-2 rounded-sm" value={taskDescription} onChange={(e) => setTaskDecriptiom(e.target.value)}>Create Task</button>
                 </div>
 
 
--- frontend/src/components/other/TaskStatusCard.jsx
+++ frontend/src/components/other/TaskStatusCard.jsx
@@ -1,11 +1,10 @@
 
 const TaskStatusCard = ({ empData }) => {
-    console.log(empData.tasks)
     return (
         <>
-            {empData.tasks.map((task) => {
+            {empData.tasks.map((task, idx) => {
                 return (
-                    <div className="w-full max-w-88 space-y-4 rounded-md border border-gray-200 bg-gray-200 p-3 text-black transition-all duration-300 hover:-translate-y-1">
+                    <div className="w-full max-w-88 space-y-4 rounded-md border border-gray-200 bg-gray-200 p-3 text-black transition-all duration-300 hover:-translate-y-1" key={idx}>
                         <div className="flex items-center justify-between">
                             <div className="flex gap-1 flex-col justify-center">
                                 <h2 className="bg-yellow-300 p-1 rounded text-black font-medium flex justify-center">{task.category}</h2>
@@ -14,7 +13,7 @@ const TaskStatusCard = ({ empData }) => {
                             <p>{task.taskDate}</p>
                         </div>
                         <p>{task.taskDescription}</p>
-                        <div className="flex items-center gap-2 pt-3 justify-center">
+                        <div className="flex items-end gap-2 pt-3 justify-end">
                             <h2 className={` p-1 rounded-full text-black font-medium w-1/2 flex justify-center ${task.active ? "active" : (task.completed ? "completed" : "failed")}`}>{task.active ? "Active" : (task.completed ? "Completed" : "Failed")}</h2>
                         </div>
                     </div>
